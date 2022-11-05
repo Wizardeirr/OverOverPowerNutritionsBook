8e96dd2bab390b17b67abd0be9522facac4d3ee8
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2022-11-05 12:20:26 +0300

    Initial commit

---
+++ app/src/main/java/com/volkankelleci/oop/NutritionContent.kt
@@ -0,0 +1,34 @@
+package com.volkankelleci.oop
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_nutrition_content.*
+
+class NutritionContent : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_nutrition_content, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        liste_button.setOnClickListener {
+            val action=NutritionContentDirections.contentToList()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/volkankelleci/oop/NutritionList.kt
@@ -0,0 +1,38 @@
+package com.volkankelleci.oop
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_nutrition_content.*
+import kotlinx.android.synthetic.main.fragment_nutrition_list.*
+
+class NutritionList : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_nutrition_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        icerik_button.setOnClickListener {
+            val action=NutritionListDirections.listToContent()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+
+
+
+}
