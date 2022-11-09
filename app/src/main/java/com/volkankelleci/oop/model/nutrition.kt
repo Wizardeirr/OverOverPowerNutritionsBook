f7ac2e478f091ca5e1afb7cba70920aecaa90af7
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2022-11-09 10:59:21 +0300

    API Interface done

--- app/src/main/java/com/volkankelleci/oop/model/nutrition.kt
+++ app/src/main/java/com/volkankelleci/oop/model/nutrition.kt
@@ -1,11 +1,19 @@
 package com.volkankelleci.oop.model
 
+import com.google.gson.annotations.SerializedName
+
 data class nutrition(
+    @SerializedName("isim")
     val nutritionName:String?,
+    @SerializedName("kalori")
     val nutritionkcal:String?,
+    @SerializedName("karbonhidrat")
     val nutritionCarbonhydrate:String?,
+    @SerializedName("protein")
     val nutritionProtein:String?,
+    @SerializedName("yag")
     val nutritionFat:String?,
+    @SerializedName("gorsel")
     val nutritionImage:String?,
 ) {
 }
